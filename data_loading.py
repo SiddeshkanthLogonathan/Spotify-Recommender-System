7ba356325458f0b1ea1c9f69a3ef6fff22a21dd7
Siddeshkanth <logonathan@uni-potsdam.de>
2020-07-02 14:46:02 +0200

    Update data_loading.py
    Stored data as pandas Dataframes

--- data_loading.py
+++ data_loading.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
+# The Data Frames are named according to its csv-filename in the format of pandas-DataFrame
+data = pd.read_csv('./data/data.csv')
+data_by_artist = pd.read_csv('./data/data_by_artist.csv')
+data_by_genres = pd.read_csv('./data/data_by_genres.csv')
+data_by_year = pd.read_csv('./data/data_by_year.csv')
+data_w_year = pd.read_csv('./data/data_w_genres.csv')
+
 
