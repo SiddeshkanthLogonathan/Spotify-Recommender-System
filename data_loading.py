8ef9e006d1176d7ee6763f4fd38d35650284f243
Siddeshkanth <logonathan@uni-potsdam.de>
2020-07-03 10:49:27 +0200

    Revert "Update data_loading.py"
    This reverts commit a9d26f61fc25dd6c33b9c8c0abd79f3d0d1d1eb2.

--- data_loading.py
+++ data_loading.py
@@ -1,29 +1,10 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # The Data Frames are named according to its csv-filename in the format of pandas-DataFrame
 data = pd.read_csv('./data/data.csv')
-# data_by_artist = pd.read_csv('./data/data_by_artist.csv')
-# data_by_genres = pd.read_csv('./data/data_by_genres.csv')
-# data_by_year = pd.read_csv('./data/data_by_year.csv')
-# data_w_year = pd.read_csv('./data/data_w_genres.csv')
+data_by_artist = pd.read_csv('./data/data_by_artist.csv')
+data_by_genres = pd.read_csv('./data/data_by_genres.csv')
+data_by_year = pd.read_csv('./data/data_by_year.csv')
+data_w_year = pd.read_csv('./data/data_w_genres.csv')
 
 
-# Remove unnecessary attributes ['explicit', 'id', 'year', 'release_date']
-columns_to_drop = ['explicit', 'id', 'release_date']
-data.drop(columns_to_drop, axis=1, inplace=True)
-
-
-def min_max_normalize(df, columns):
-    for col in columns:
-        max_value = data[col].max()
-        min_value = data[col].min()
-        data[col] = (data[col] - min_value) / (max_value - min_value)
-    return df
-
-
-columns_to_normalize = ['key', 'loudness', 'popularity', 'tempo', 'speechiness', 'year']
-min_max_normalize(data, columns_to_normalize)
-
-data['year'].plot.hist(bins=10)
-plt.show()
