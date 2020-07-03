a7f694c3f60f1d39c7d43760f9a91265d865a409
Jeffrey Johnson <jojohnso101@gmail.com>
2020-07-03 10:01:35 +0200

    initial progress

--- data_loading.py
+++ data_loading.py
@@ -1,10 +1,27 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
 
-# The Data Frames are named according to its csv-filename in the format of pandas-DataFrame
+# Read data from .csv
 data = pd.read_csv('./data/data.csv')
-data_by_artist = pd.read_csv('./data/data_by_artist.csv')
-data_by_genres = pd.read_csv('./data/data_by_genres.csv')
-data_by_year = pd.read_csv('./data/data_by_year.csv')
-data_w_year = pd.read_csv('./data/data_w_genres.csv')
+# print(data.columns)
+# print(data.head(5))
 
+# Drop irrelevant data
+data.drop(['duration_ms', 'release_date', 'id'], axis=1, inplace=True)
 
+# Normalize columns
+data['popularity'] = data['popularity']/100
+data['tempo'] = (data['tempo'] - 50)/100
+data['loudness'] = (data['loudness'] + 60)/60
+
+# Visualization
+# print(data)
+# plt.scatter(x=data['danceability'],
+#             y=data['popularity'])
+# plt.show()
+
+# Convert to tensor
+t = data.to_numpy()
+print(t.shape)
