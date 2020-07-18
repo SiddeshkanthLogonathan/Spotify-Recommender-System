bda206265546b1fe73caf2e2f5dc7a8fc2504ebe
jostmorgenstern <Jost.Morgenstern@student.hpi.uni-potsdam.de>
2020-07-18 13:59:55 +0200

    dataset returns indices for genres and artists, distinct numer for songs without genres

--- data_loading.py
+++ data_loading.py
@@ -1,41 +1,43 @@
-from typing import Tuple, Union
-from pandas import read_csv, DataFrame, Series, read_pickle
+import pandas as pd
+import torch
+import os
 from torch.utils.data import Dataset
-from torch import tensor
+from typing import Tuple, Union
 from ast import literal_eval
 from itertools import chain
 from tqdm import tqdm
-from sklearn.preprocessing import MultiLabelBinarizer
-from torch import tensor
-import os
+
 
 class SpotifyRecommenderDataset(Dataset):
     COLUMNS_TO_DROP = ['explicit', 'id', 'release_date', 'name']
-    COLUMNS_TO_NORMALIZE = ['key', 'loudness', 'popularity', 'tempo', 'speechiness', 'year', 'duration_ms']
+    NUMERIC_COLUMNS = ["acousticness", "danceability", "duration_ms", "energy", "instrumentalness", "key",
+                            "liveness", "loudness", "mode", "popularity", "speechiness", "tempo", "valence"]
+    DISTINCT_ARTISTS_COUNT = 27621
+    DISTINCT_GENRES_COUNT = 2663
+    NUMERIC_FIELDS_COUNT = 14
 
     def __init__(self, data_path='data/data.csv', data_w_genres_path='data/data_w_genres.csv',
-                       data_by_genres_path='data/data_by_genres.csv', pickle_path='data/dataset.pkl'):
+                 data_by_genres_path='data/data_by_genres.csv', pickle_path='data/dataset.pkl'):
         """Because the dataset is stored once it is created, only the first initialization takes long."""
 
-        self.df_w_genres = read_csv(data_w_genres_path)
-        self.df_by_genres = read_csv(data_by_genres_path)
+        self.df_w_genres = pd.read_csv(data_w_genres_path)
+        self.df_by_genres = pd.read_csv(data_by_genres_path)
         self._convert_string_column_to_list_type(self.df_w_genres, 'genres')
 
-        if os.path.exists(pickle_path):        
-            self.df = read_pickle(pickle_path)
+        if os.path.exists(pickle_path):
+            self.df = pd.read_pickle(pickle_path)
         else:
-            self.df = read_csv(data_path)
+            self.df = pd.read_csv(data_path)
             self._convert_string_column_to_list_type(self.df, 'artists')
-            self._drop_columns()
+            self._drop_unnecessary_columns()
             self.df['genres'] = self._genres_column()
-            self._normalize_columns()
+            self._numerize_genres_and_artists_columns()
+            self._normalize_numeric_columns()
             self.df.to_pickle(pickle_path)
 
-        self._setup_mlbs()
-
-    def _genres_column(self) -> Series:
+    def _genres_column(self) -> pd.Series:
         """All unique genres of a song. The genres of a song are the genres of all artists of the song."""
-        
+
         genres_column = []
 
         for i in tqdm(range(len(self.df.index)), desc="Collecting genres"):
@@ -45,27 +47,50 @@ class SpotifyRecommenderDataset(Dataset):
             genres_of_song = list(set(chain.from_iterable(genres_of_song)))
             genres_column.append(genres_of_song)
 
-        return Series(genres_column)
+        return pd.Series(genres_column)
 
-    def _setup_mlbs(self):
-        """For one-hot/multiple-hot vectors. It would be insane to store these."""
+    def _numerize_genres_and_artists_columns(self):
+        distinct_genres = list(self.df_by_genres['genres'])
+        genre_for_songs_without_genres = len(distinct_genres)
+        numerized_genres_column = []
 
-        artists = set(self.df_w_genres['artists'])
-        self.artists_mlb = MultiLabelBinarizer().fit([artists])
-        
-        genres = set(self.df_by_genres['genres'].drop(index=1))
-        self.genres_mlb = MultiLabelBinarizer().fit([genres])
-        
-    def _convert_string_column_to_list_type(self, dataframe: DataFrame, column: str):
+        for i in tqdm(range(len(self.df.index)), desc="Numerizing genres"):
+            genres = self.df.loc[i, 'genres']
+            if genres:
+                numerized_genres = [distinct_genres.index(genre) for genre in genres]
+            else:
+                numerized_genres = [genre_for_songs_without_genres]
+                genre_for_songs_without_genres += 1
+
+            numerized_genres_column.append(numerized_genres)
+
+        self.df['genres'] = numerized_genres_column
+
+        distinct_artists = list(self.df_w_genres['artists'])
+        artist_index_for_songs_without_artist = len(distinct_artists)
+        numerized_artists_column = []
+        for i in tqdm(range(len(self.df.index)), desc="Numerizing artists"):
+            artists = self.df.loc[i, 'artists']
+            numerized_artists = [distinct_artists.index(artist) for artist in artists if artist in distinct_artists]
+
+            if not numerized_artists:
+                numerized_artists = [artist_index_for_songs_without_artist]
+                artist_index_for_songs_without_artist += 1
+
+            numerized_artists_column.append(numerized_artists)
+
+        self.df['artists'] = numerized_artists_column
+
+    def _convert_string_column_to_list_type(self, dataframe: pd.DataFrame, column: str):
         string_column = dataframe[column]
         list_column = [literal_eval(string_element) for string_element in string_column]
         dataframe[column] = list_column
 
-    def _drop_columns(self):
+    def _drop_unnecessary_columns(self):
         self.df.drop(self.COLUMNS_TO_DROP, axis=1, inplace=True)
 
-    def _normalize_columns(self):
-        for col in self.COLUMNS_TO_NORMALIZE:
+    def _normalize_numeric_columns(self):
+        for col in self.NUMERIC_COLUMNS:
             max_value = self.df[col].max()
             min_value = self.df[col].min()
             self.df[col] = (self.df[col] - min_value) / (max_value - min_value)
@@ -73,17 +98,11 @@ class SpotifyRecommenderDataset(Dataset):
     def __len__(self):
         return len(self.df.index)
 
-    def __getitem__(self, idx: Union[int, slice, list]) -> Tuple[tensor, tensor, tensor]:      
+    def __getitem__(self, idx: Union[int, slice, list]) -> Tuple[torch.tensor, torch.tensor, torch.tensor]:
         numeric_fields = self.df.loc[idx, self.df.columns.difference(['artists', 'genres'])]
-        numeric_fields_tensor = tensor(numeric_fields.values.astype('float64'))
-        
+        numeric_fields_tensor = torch.tensor(numeric_fields.values.astype('float32')).squeeze()
+
         artists = list(self.df.loc[idx, 'artists'])
         genres = list(self.df.loc[idx, 'genres'])
-        if type(idx) == int:
-            artists = [artists]
-            genres = [genres]
-        
-        artists_tensor = tensor(self.artists_mlb.transform(artists))
-        genres_tensor = tensor(self.genres_mlb.transform(genres))
 
-        return (artists_tensor, numeric_fields_tensor, genres_tensor)
+        return artists, numeric_fields_tensor, genres
