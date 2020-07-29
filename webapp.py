0d7ab794d577e06acbaae98f2db58964152b5842
jostmorgenstern <Jost.Morgenstern@student.hpi.uni-potsdam.de>
2020-07-29 11:17:23 +0200

    improve webapp

---
+++ webapp.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import plotly.express as px  # (version 4.7.0)
+import plotly.graph_objects as go
+
+from data_loading import SpotifyRecommenderDataset
+
+import dash  # (version 1.12.0) pip install dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_table
+import pandas as pd
+import torch
+import numpy as np
+import datetime
+
+raw_dataset = pd.read_csv('data/data.csv')
+raw_dataset = raw_dataset[['name', 'artists', 'release_date', 'duration_ms', 'popularity']]
+
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+
+    html.H1("Spotify Recommender Webapp", style={'text-align': 'center'}),
+
+    html.H3("Enter a song name"),
+    dcc.Input(id='song_name_input', value="Fantasiestücke, Op. 111: Più tosto lento"),
+    html.H3("Enter list of artists"),
+    dcc.Input(id='artists_input', value="['Robert Schumann', 'Vladimir Horowitz']"),
+    html.Br(),
+
+    dcc.Graph(id='3d_scatter_plot', figure={}),
+
+    dash_table.DataTable(
+        columns=[{'name': column_name,
+                  'id': column_name,
+                  'type': ('text' if column_name in ['name', 'artists'] else 'numeric')}
+                 for column_name in raw_dataset.columns],
+        data=raw_dataset.to_dict('records'),
+        filter_action='native',
+        style_table={
+            'height': 400,
+        },
+        style_data={
+            'width': '150px', 'minWidth': '150px', 'maxWidth': '150px',
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+        }
+    )
+])
+
+
+# ------------------------------------------------------------------------------
+# Connect the Plotly graphs with Dash Components
+@app.callback(
+    # [Output(component_id='output_container', component_property='children'),
+    Output(component_id='3d_scatter_plot', component_property='figure'),
+    [Input(component_id='song_name_input', component_property='value'),
+     Input(component_id='artists_input', component_property='value')]
+)
+def update_graph(song_name, artists_string):
+    print(song_name)
+    print(artists_string)
+
+    df = pd.read_csv('data/data.csv')
+    df = df.head(100)
+
+    encoding_x_column = np.random.randint(0, 100, 100)
+    encoding_y_column = np.random.randint(0, 100, 100)
+    encoding_z_column = np.random.randint(0, 100, 100)
+
+    df['encoding_x_column'] = encoding_x_column
+    df['encoding_y_column'] = encoding_y_column
+    df['encoding_z_column'] = encoding_z_column
+    # df['color_column'] = ['lightskyblue'] * 42 + ['red'] * 11 + ['mediumpurple'] * 47
+    df['color_column'] = list(range(80, 100)) + [0] * 80
+    df['symbol_column'] = [0] + (len(df) - 1) * [1]
+
+    data_point = df[(df.name == song_name) & (df.artists == artists_string)]
+    print(data_point)
+
+    # df = px.data.iris()
+    fig = px.scatter_3d(df, x='encoding_x_column', y='encoding_y_column', z='encoding_z_column', hover_name="name",
+                        hover_data=df.columns, color='color_column', symbol='symbol_column')
+
+    return fig
+
+
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app.run_server(debug=True)
