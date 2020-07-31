d9d8fc3114bc16c166a59bbd1b4987427a19a897
Snagnar <overkill98@web.de>
2020-07-31 16:16:32 +0200

    added some command line args to main

--- train.py
+++ train.py
@@ -4,8 +4,7 @@ import torch
 
 
 
-def train_autoencoder(model: Autoencoder, dataloader, verbose=True):
-    num_epochs = 10
+def train_autoencoder(model: Autoencoder, dataloader, verbose=True, num_epochs=10):
     criterion = torch.nn.MSELoss()
     learning_rate = 0.01
     # default_decay_rate = 0.9
@@ -32,6 +31,6 @@ def train_autoencoder(model: Autoencoder, dataloader, verbose=True):
         epoch_loss = epoch_loss / len(dataloader)
         #scheduler.step()
         if verbose:
-            print(f"Loss of epoch {epoch}: {epoch_loss}")
+            print(f"Loss of epoch {epoch+1}: {epoch_loss}")
 
     return model
